a7447ff194c4090b8994e7d877b2b389b92697bc
Meerebrise <12321>
2016-02-13 01:17:35 +0800

    16/2/13深夜代码提交

---
+++ C++ Primer/Codes/Code_C07/CppPrimerClass/CppPrimerClass/Sales_data.cpp
@@ -0,0 +1,19 @@
+#include "Sales_data.h"
+#include <iostream>
+
+using namespace::std;
+
+Sales_data::Sales_data(std::istream &is)
+{
+
+}
+
+Sales_data& Sales_data::combine(const Sales_data&)
+{
+
+}
+
+double Sales_data::avg_price() const
+{
+
+}
